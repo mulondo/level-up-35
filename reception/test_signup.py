ad95c3b5ff765ae1b58e30734ec4ddc92f26c0fe
mulondo <mulondo26@gmail.com>
2018-09-05 19:27:34 +0300

    add more tests

--- reception/test_signup.py
+++ reception/test_signup.py
@@ -16,7 +16,7 @@ class Test(unittest.TestCase):
 
     def test_get_user(self):
         self.sign_up.add_user("mulondo", "12345678","mulo@gmail.com")
-        self.assertTrue(self.sign_up.get_user("moses"), msg="The user name is not registered")
+        self.assertTrue(self.sign_up.get_user("moses","mulo@gmail.com"), msg="The user name is not registered")
 
     def test_validation_input(self):
         self.assertTrue(self.sign_up.validate_input(254, 9932), msg="wrong input format")
@@ -34,4 +34,8 @@ class Test(unittest.TestCase):
     def test_forgot_username(self):
        self.sign_up.add_user("paul", "123456","paul@gmail.com")
        self.assertTrue(self.sign_up.forgot_username("paul@gmail.com","123456"),msg="wrong email or password")
-       
+    
+    def test_remove_user(self):
+        self.sign_up.add_user("paul", "123456","paul@gmail.com")
+        self.assertTrue(self.sign_up.remove_user("paul"),msg="The user was not found")
+    
