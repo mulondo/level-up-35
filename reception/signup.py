912da1256c2247756b9399b33b1d830a54c67978
mulondo <mulondo26@gmail.com>
2018-09-05 18:02:10 +0300

    add more methods

--- reception/signup.py
+++ reception/signup.py
@@ -1,14 +1,20 @@
 class Signup:
     def __init__(self):
-        self.users={}
+        self.users=[]
 
-    def add_user(self, username, password):
-        self.users[username] = password
+    def add_user(self, username, password, email):
+        reg_user={
+            "email":email,
+            "username":username,
+            "password":password
+        }
+        self.users.append(reg_user)
         return self.users
 
     def get_user(self, username):
-        if self.users[username]:
-            return self.users
+        for usr in self.users:
+            if usr['username']==username:
+                return self.users
         else:
             return "Wrong user name or password"
     
@@ -22,9 +28,16 @@ class Signup:
         return len(self.users)
 
     def change_password(self,username, newpassord):
-        self.users[username]=newpassord
-        return  self.users
-
+        for usrs in self.users:
+            if usrs['username']==username:
+                usrs['password']=newpassord
+        return  self.users    
+    
+    def forgot_username(self,email,password):
+        for user in self.users:
+            if (user['email']==email and user['password']==password):
+                return self.users
+        return "incorrect email or password"
 
 
 
