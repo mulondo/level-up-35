52f5ed1852102a8308714ef19a2cce87e2159b48
mulondo <mulondo26@gmail.com>
2018-09-06 10:15:07 +0300

    edit validation method

--- reception/signup.py
+++ reception/signup.py
@@ -14,37 +14,39 @@ class Signup:
     def get_user(self, username,password):
         for usr in self.users:
             if usr['username']==username and usr['email']==password:
-                return self.users
+                return usr
         else:
             return "Wrong user name or password"
     
-    def validate_input(self, username, password):
-        if username or password is not str:
-            return "wrong input format"
-        elif username or password is None:
-            return "Username or password is empty"
-
+    def validate_input(self, username, password):        
+        if type(username) is not str or type(password) is not str:
+            raise TypeError("incorrect input")
+        else:
+            return "right format"
+            
     def check_length(self):
         return len(self.users)
 
     def change_password(self,username, newpassord):
         for usrs in self.users:
-            if usrs['username']==username:
+            if  usrs['username']==username:
                 usrs['password']=newpassord
-        return  self.users
+                return  usrs
 
     def remove_user(self,username):
         for usr in self.users:
             if usr['username']==username:
                 usr['username']="" 
-                return self.users
+                return usr
         return "user doesnot exist" 
     
     def forgot_username(self,email,password):
-        for user in self.users:
-            if (user['email']==email and user['password']==password):
-                return self.users
+        for ur in self.users:
+            if ur['email'] == email:
+                return ur
         return "incorrect email or password"
+# people=Signup()
+# print(people.validate_input("1322","34567"))
     
     
 
